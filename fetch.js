3b71b77db378358e9173eb6e6ccd19ef6359f472
manonvv1 <vlie0073@hz.nl>
2023-06-20 08:44:42 +0200

    working aapi

---
+++ fetch.js
@@ -0,0 +1,34 @@
+function shiba() {
+
+    const data = fetch('http://shibe.online/api/shibes?count='+ shibaCount, {
+        headers: {
+            Accept: "application/json; charset=utf-8",
+        },
+        mode: "cors"
+    })
+        .then(response => response.json())
+        .then(function (text){
+            const body = document.querySelector('body');
+            for(let i = 0; i < text.length; i++){
+                const woof = document.createElement('div');
+                woof.className = 'profile';
+
+                const imageElement = document.createElement('img');
+                imageElement.className = 'portfolio'
+                imageElement.src = text[i];
+
+                woof.appendChild(imageElement);
+                body.appendChild(woof);
+            }
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        });
+}
+window.onload = shiba;
+
+function refreshPageWithRandomNumber() {
+  let randomNumber = Math.floor(Math.random() * 50) + 1; // Generate a random number between 1 and 50
+  window.location.href = window.location.href.split('?')[0] + "?" + randomNumber; // Refresh the page with the new URL
+}
